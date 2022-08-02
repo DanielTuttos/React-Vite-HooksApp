6948c120372fa56661bbafdc19443d4c039cf2c8
Daniel Romero AG <tutorialesdanielromero@gmail.com>
2022-08-02 11:51:31 -0500

    useEffect and useFecth custom hook for request

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <FormWithCustomHook />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <MultipleCustomHooks />
+  </React.StrictMode>
 )
