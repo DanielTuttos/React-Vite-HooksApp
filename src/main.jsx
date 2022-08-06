bb716a4623537547d4ca8f06abedca22221fedbd
Daniel Romero AG <tutorialesdanielromero@gmail.com>
2022-08-06 15:46:23 -0500

    Introduccion a Reducer

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,12 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+
+import './08-useReducer/intro-reducer';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre />
+//   // </React.StrictMode>
+// )
